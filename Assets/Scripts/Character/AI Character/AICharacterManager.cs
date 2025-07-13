0329b18142554cff6c0aa966394f3c7237de6744
EnfieldL42 <84160515+EnfieldL42@users.noreply.github.com>
2025-07-13 21:00:21 +1200

    fixed aistates

--- Assets/Scripts/Character/AI Character/AICharacterManager.cs
+++ Assets/Scripts/Character/AI Character/AICharacterManager.cs
@@ -66,7 +66,8 @@ public class AICharacterManager : CharacterManager
 
     private void ProcessStateMachine()
     {
-        AIState nextState = currentState?.Tick(this);
+        //AIState nextState = currentState?.Tick(this);
+        AIState nextState = currentState != null ? currentState.Tick(this) : null;
 
         if (nextState != null)
         {
