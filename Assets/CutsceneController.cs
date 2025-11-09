f6e9a0efc872702caf6c8b15c6061dea5538bc01
ry07a <harry.situ4@gmail.com>
2025-11-10 11:19:25 +1300

    player hidden during cutscene

--- Assets/CutsceneController.cs
+++ Assets/CutsceneController.cs
@@ -2,8 +2,37 @@ using UnityEngine;
 
 public class CutsceneController : MonoBehaviour
 {
+    private void Start()
+    {
+        PlayerManager player = FindAnyObjectByType<PlayerManager>();
+        SkinnedMeshRenderer[] meshes = player.GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (SkinnedMeshRenderer mesh in meshes)
+        {
+            mesh.enabled = false;
+        }
+        MeshRenderer[] weaponMeshes = player.GetComponentsInChildren<MeshRenderer>();
+        foreach(MeshRenderer weapon in weaponMeshes)
+        {
+            weapon.enabled = false;
+        }
+        PlayerUIHudManager hudManager = FindAnyObjectByType<PlayerUIHudManager>();
+        hudManager.ToggleHUD(false);
+    }
     public void EndIntroCutscene()
     {
+        PlayerManager player = FindAnyObjectByType<PlayerManager>();
+        SkinnedMeshRenderer[] meshes = player.GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (SkinnedMeshRenderer mesh in meshes)
+        {
+            mesh.enabled = true;
+        }
+        MeshRenderer[] weaponMeshes = player.GetComponentsInChildren<MeshRenderer>();
+        foreach (MeshRenderer weapon in weaponMeshes)
+        {
+            weapon.enabled = true;
+        }
         WorldSaveGameManager.instance.AttemptToCreateNewGame();
+        PlayerUIHudManager hudManager = FindAnyObjectByType<PlayerUIHudManager>();
+        hudManager.ToggleHUD(true);
     }
 }
