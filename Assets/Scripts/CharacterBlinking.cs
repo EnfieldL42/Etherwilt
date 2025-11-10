83de732ea71cea9fdec3ee8fc0fcf16dad736cca
ry07a <harry.situ4@gmail.com>
2025-11-10 18:26:13 +1300

    character blinking

---
+++ Assets/Scripts/CharacterBlinking.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CharacterBlinking : MonoBehaviour
+{
+    [SerializeField] SkinnedMeshRenderer[] meshes;
+    [Header("Blink Properties")]
+    private float blinkInterval = 5.0f;
+    private float blinkEyeCloseDuration = 0.05f;
+    private float blinkOpeningDuration = 0.1f;
+    private float blinkClosingDuration = 0.1f;
+
+    private void OnEnable()
+    {
+        StartCoroutine(BlinkCoroutine());  
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(BlinkCoroutine());
+    }
+    IEnumerator BlinkCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(blinkInterval);
+            var value = 0f;
+            var closeSpeed = 1f / blinkClosingDuration;
+            while (value < 1)
+            {
+                foreach (var mesh in meshes)
+                {
+                    mesh.SetBlendShapeWeight(0, value * 100);
+                    value += Time.deltaTime * closeSpeed;
+                    yield return null;
+                }
+            }
+            foreach (var mesh in meshes)
+            {
+                mesh.SetBlendShapeWeight(0, 100);
+            }
+
+            yield return new WaitForSeconds(blinkEyeCloseDuration);
+
+            value = 1f;
+            var openSpeed = 1f / blinkOpeningDuration;
+            while (value > 0)
+            {
+                foreach (var mesh in meshes)
+                {
+                    mesh.SetBlendShapeWeight(0, value * 100);
+                    value -= Time.deltaTime * openSpeed;
+                    yield return null;
+                }
+            }
+            foreach (var mesh in meshes)
+            {
+                mesh.SetBlendShapeWeight(0, 0);
+            }
+        }
+    }
+}
